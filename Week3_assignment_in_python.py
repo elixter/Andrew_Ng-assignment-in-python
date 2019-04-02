d0623409cf819bd07c884fea17222e8f96887ba3
elixter <ltw971@naver.com>
2019-04-02 13:51:34 +0900

    기본적인 Logistic Regression 구현

--- Week3_assignment_in_python.py
+++ Week3_assignment_in_python.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 
 
 # sigmoid function
@@ -9,12 +8,8 @@ def sigmoid(z):
     return g
 
 
-def sigmoid(z):
-    g = 1 / (1 + math.exp(-z))
-
-
 def hypothesis(theta, x):
-    return sigmoid(np.multiply(theta, x))
+    return sigmoid(np.sum(np.transpose(theta) * x))
 
 
 # costFunction for Logistic Regression
@@ -22,23 +17,48 @@ def costFunction(theta, x, y):
     m = x.shape[0]
     sum = 0
     for i in range(m):
-        sum += y[i] * np.log(hypothesis(theta, x[i])) - (1 - y[i]) * np.log(1 - hypothesis(theta, x[i]))
+        sum += -y[i] * np.log(hypothesis(theta, x[i])) - (1 - y[i]) * np.log(1 - hypothesis(theta, x[i]))
 
     return sum / m
 
 
 # theta j 에 대한 편미분값
-def deriveCost(theta, x, y):
+def deriveCost(theta, x, y, j):
     m = x.shape[0]
+    n = x.shape[1]
     J = 0
 
     for i in range(m):
-        J += (hypothesis(theta, x[i]) - y[i]) * x[i]
+        J += (hypothesis(theta, x[i]) - y[i]) * x[i][j]
 
     return J / m
 
 
-#def Gradient_Descent(function):
+# alpha == learning rate
+def Gradient_Descent(theta, x, y, alpha=0.001):
+    n = theta.shape[0]
+    tmp = np.zeros(theta.shape, dtype=np.float32)
+    for i in range(n):
+        tmp[i][0] = theta[i][0] - alpha * deriveCost(theta, x, y, i)
+    print(theta)
+    theta = tmp
+    print(theta)
+    return theta
+
+
+def Logistic_Regression(theta, x, y, epoch=400, alpha=0.001):
+    tmp = costFunction(theta, x, y)
+    for i in range(epoch):
+        tmp_theta = Gradient_Descent(theta, x, y, alpha)
+        tmp2 = costFunction(tmp_theta, x, y)
+        print([tmp, tmp2])
+
+        if tmp > tmp2:
+            theta = tmp_theta
+            tmp = tmp2
+
+    return theta
+
 
 
 
--- assignment test.py
+++ assignment test.py
@@ -8,21 +8,21 @@ data = []
 with open('resource/ex2data1.csv', 'r', encoding='UTF8') as file:
     csv_reader = csv.reader(file, delimiter=',')
     for row in csv_reader:
-        data_info = (row[0], row[1], row[2])
+        data_info = [float(row[0]), float(row[1]), int(row[2])]
         data.append(data_info)
 
-admit_x = []
-admit_y = []
-non_admit_x = []
-non_admit_y = []
+admit_x1 = []
+admit_x2 = []
+non_admit_x1 = []
+non_admit_x2 = []
 
 for row in data:
-    if row[2] == '1':
-        admit_x.append(float(row[0]))
-        admit_y.append(float(row[1]))
+    if row[2] == 1:
+        admit_x1.append(float(row[0]))
+        admit_x2.append(float(row[1]))
     else:
-        non_admit_x.append(float(row[0]))
-        non_admit_y.append(float(row[1]))
+        non_admit_x1.append(float(row[0]))
+        non_admit_x2.append(float(row[1]))
 
 
 # 데이터 시각화
@@ -30,9 +30,48 @@ plt.figure()
 
 plt.xlabel('Exam 1 score')
 plt.ylabel('Exam 2 score')
-plt.scatter(admit_x, admit_y, marker='+')
-plt.scatter(non_admit_x, non_admit_y, marker='o')
+plt.scatter(admit_x1, admit_x2, marker='+')
+plt.scatter(non_admit_x1, non_admit_x2, marker='o')
 plt.show()
 
 
-print(np.multiply([1, 2, 3, 4,], [2, 2, 2, 2]))
+initial_theta = np.zeros([len(data[0]), 1], dtype=np.float32)
+test_theta = np.array([[-24],
+                       [0.2],
+                       [0.2]])
+
+x_data = []
+y_data = []
+
+for row in data:
+    set = [1, row[0], row[1]]
+    x_data.append(set)
+    y_data.append(row[2])
+
+result = week3.costFunction(initial_theta, np.array(x_data), np.array(y_data))
+print(result)
+
+x = range(100)
+
+
+result_theta = test_theta
+
+week3.deriveCost(initial_theta, np.array(x_data), np.array(y_data), 0)
+week3.deriveCost(initial_theta, np.array(x_data), np.array(y_data), 1)
+week3.deriveCost(initial_theta, np.array(x_data), np.array(y_data), 2)
+
+# for i in range(10):
+#     result_theta = week3.Gradient_Descent(result_theta, np.array(x_data), np.array(y_data), alpha=0.001)
+
+result_theta = week3.Logistic_Regression(result_theta, np.array(x_data), np.array(y_data), alpha=0.001)
+
+print(result_theta)
+
+plt.figure()
+plt.xlabel('Exam 1 score')
+plt.ylabel('Exam 2 score')
+plt.scatter(admit_x1, admit_x2, marker='+')
+plt.scatter(non_admit_x1, non_admit_x2, marker='o')
+plt.axis([0, 100, 0, 100])
+plt.plot(x, -((result_theta[0][0] + result_theta[1][0] * x) / result_theta[2][0]))
+plt.show()
